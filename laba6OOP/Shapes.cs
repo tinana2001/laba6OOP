cf6016c2b64e45292ec6dcad4204f5c695929cbf
nastya <tinana2013@gmail.com>
2021-02-14 13:59:38 +0500

    Добавьте файлы проекта.

---
+++ laba6OOP/Shapes.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace laba6OOP
+{
+    public class Shape
+    {
+
+    }
+    
+    public class Circle
+    {
+        public Color color = Color.DeepPink;
+        public int x, y, r;
+        public bool flag = false;
+        public Circle(int x1, int y1, int r1)
+        {
+            x = x1;
+            y = y1;
+            r = r1;
+        }
+
+        public bool CheckPoint(int x, int y)
+        {
+            if (((this.x - x) * (this.x - x) + (this.y - y) * (this.y - y)) <= r * r)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void Draw(Graphics graph)
+        {
+            Pen pen;
+
+            if (flag)
+            {
+                pen = new Pen(color, 10);
+            }
+            else
+            {
+                pen = new Pen(color);
+            }
+            graph.DrawEllipse(pen, x - r, y - r, 2 * r, 2 * r);
+        }
+
+    }
+}
---
+++ laba6OOP/Storage.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laba6OOP
+{
+    class Storage
+    {
+        public Shape[] _values;
+        int size;
+        int count = 0;
+
+        public Storage(int size)
+        {
+            this.size = size;
+            _values = new Shape[size];
+        }
+
+
+        public int getCount()
+        {
+            return count;
+        }
+
+        public Shape getObj(int i)
+        {
+            if (i < count) return _values[i];
+            return null;
+        }
+
+
+        public void DeleteItem(int index)
+        {
+            if (size > 0)
+            {
+                if (index <= size)
+                {
+                    for (int i = index; i < count; i++)
+                    {
+                        _values[i] = _values[i + 1];
+                    }
+                    count--;
+                    Array.Clear(_values, count, 1);
+
+                }
+            }
+
+        }
+        private bool CheckFree() // проверка свободных мест
+        {
+            if (count < size)
+            {
+                return true;
+            }
+            return false;
+        }
+
+
+        public void AddElem()
+        {
+            Shape[] _bufer = new Shape[size * 2 + 1];
+            for (int i = 0; i < size; i++)
+            {
+                _bufer[i] = _values[i];
+            }
+            _values = _bufer;
+            size = size * 2;
+        }
+        public void CreatItem(Shape item)
+        {
+            if (!CheckFree())
+            {
+                AddElem();
+            }
+            _values[count] = item;
+            count++;
+        }
+    }
+}
